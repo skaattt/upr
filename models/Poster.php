1e808af5e51c7d5ad06973b5dcd9d7636b6bbb57
Ekaterina Semyonova <katkatvww@mail.ru>
2024-03-03 16:46:22 +0300

    Регистрация и авторизация

---
+++ models/Poster.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "poster".
+ *
+ * @property int $id_poster
+ * @property string $name
+ * @property int $tickets
+ *
+ * @property Order[] $orders
+ */
+class Poster extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'poster';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'tickets'], 'required'],
+            [['tickets'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_poster' => 'Id Poster',
+            'name' => 'Наименование мероприятия',
+            'tickets' => 'Количество свободных билетов',
+        ];
+    }
+
+    /**
+     * Gets query for [[Orders]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrders()
+    {
+        return $this->hasMany(Order::class, ['id_poster' => 'id_poster']);
+    }
+}
